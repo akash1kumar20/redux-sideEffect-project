aa7ad1f19ecb8c1d26298aebd72f6dbe26481a0d
akash1kumar20 <akashkumar2101sm@gmail.com>
2023-08-14 14:15:05 +0530

    GET Request

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,11 +1,31 @@
 import classes from "./CartItem.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "../../redux/cartSlice";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const CartItem = (props) => {
+  const [data, setData] = useState([]);
   const dispatch = useDispatch();
-
   const { title, quantity, total, price, id } = props.item;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let res = await axios.get(
+          "https://shoe-website-7aab6-default-rtdb.firebaseio.com/cart.json"
+        );
+        const dataExtracted = [];
+        for (let key in res.data) {
+          dataExtracted.push({ ...res.data, id: key });
+        }
+        setData(res.data.items);
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
   const increaseQuantity = (event) => {
     event.preventDefault();
     dispatch(
@@ -20,25 +40,30 @@ const CartItem = (props) => {
     event.preventDefault();
     dispatch(cartActions.decrease(id));
   };
+
   return (
-    <li className={classes.item}>
-      <header>
-        <h3>{title}</h3>
-        <div className={classes.price}>
-          ${total}
-          <span className={classes.itemprice}>(${price})</span>
-        </div>
-      </header>
-      <div className={classes.details}>
-        <div className={classes.quantity}>
-          x <span>{quantity}</span>
-        </div>
-        <div className={classes.actions}>
-          <button onClick={decreaseQuantity}>-</button>
-          <button onClick={increaseQuantity}>+</button>
-        </div>
-      </div>
-    </li>
+    <>
+      {data.map((item) => (
+        <li className={classes.item} key={id}>
+          <header>
+            <h3>{item.name}</h3>
+            <div className={classes.price}>
+              ${total}
+              <span className={classes.itemprice}>(${item.price})</span>
+            </div>
+          </header>
+          <div className={classes.details}>
+            <div className={classes.quantity}>
+              x <span>{item.quantity}</span>
+            </div>
+            <div className={classes.actions}>
+              <button onClick={decreaseQuantity}>-</button>
+              <button onClick={increaseQuantity}>+</button>
+            </div>
+          </div>
+        </li>
+      ))}
+    </>
   );
 };
 
